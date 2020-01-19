a68f8f693131892bc23446cf4e48ba72c4e1fab9
pio93 <p.koloszyc@stud.uis.no>
2020-01-19 15:31:50 +0100

    login and register logic and input validation

--- del2/app.py
+++ del2/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, g, url_for, request, redirect
+from flask import Flask, render_template, g, url_for, request, redirect, flash
+from os import urandom
 import mysql.connector
 
 app = Flask(__name__)
+
+app.debug = True 
+app.secret_key = urandom(24)
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -20,61 +24,90 @@ def close_db(error):
 
 @app.route('/', methods=['GET','POST'])
 def welcome():
-    db = get_db()
-    cursor = db.cursor()
-    try:
-        cursor.execute("SELECT * FROM users")
-        for username, password, city, country, address, first_name, last_name in cursor:
-            print(username)
-            print(password)
-            print(city)
-            print(str(country))
-            print(address)
-            print(first_name)
-            print(last_name)
-    except mysql.connector.Error as err:
-        print(err)
-    finally:
-        cursor.close()
-        return render_template('welcome.html', title='welcome')
-
     return render_template('welcome.html', title='welcome')
 
 @app.route('/register', methods=['POST'])
 def register():
+       
+    fields = {'username': request.form["user"],
+     'password': request.form["password"],
+     'conf_password': request.form["conf_password"],
+     'city': request.form["city"], 
+     'address': request.form["address"], 
+     'first_name': request.form["first_name"], 
+     'last_name': request.form["last_name"]}
+    
+    resp = validate_input(fields)
+
+    if resp is not None:
+        flash(resp)
+        return render_template('welcome.html', title='welcome')
+
     sql = 'INSERT INTO Users(username, u_password, city, country, address, first_name, last_name)'\
-     'VALUE("{}", "{}", "{}", "{}", "{}", "{}", "{}");'.format(request.form["user"],request.form["password"], request.form["city"], str(request.form["country"]), request.form["address"], request.form["first_name"], request.form["last_name"])   
+     'VALUE(%s, %s, %s, %s, %s, %s, %s);'
+    
     db = get_db()
     cursor = db.cursor()
     try:
-        cursor.execute(sql)
+        cursor.execute(sql, (request.form["user"],
+                            request.form["password"], 
+                            request.form["city"], 
+                            str(request.form["country"]), 
+                            request.form["address"], 
+                            request.form["first_name"], 
+                            request.form["last_name"]))
         db.commit()
     except mysql.connector.Error as err:
-        print(err)
+        if err.errno == 1062:
+            print(err)
+            flash('Username already exists')
+        else:
+            print(err)
+            flash('Registration failed')
+
+        return render_template('welcome.html', title='welcome')
     finally:
         cursor.close()
+        print('cursor closed')
 
+    flash('Registration succesfull')
     return redirect(url_for('welcome'))
 
 @app.route('/login', methods=['POST'])
 def login(): 
-    sql = 'SELECT username, u_password FROM Users WHERE username = "{}" AND u_password = "{}"'.format(request.form["user"], request.form["password"])
+    sql = 'SELECT username, u_password FROM Users WHERE username = %s AND u_password = %s'
     db = get_db()
     cursor = db.cursor()
     try:
-        cursor.execute(sql)
-        cursor.fetchone()
+        cursor.execute(sql, (request.form["log_user"], request.form["log_password"]))
+        row = cursor.fetchone()
     except mysql.connector.Error as err:
         print(err) 
-        return redirect(url_for('welcome'))
     finally:
         cursor.close()
-    
-    return redirect(url_for('main'))
+    if row is not None:
+        return redirect(url_for('main', username=row[0]))
+    else:
+        flash('Login failed !')
+        return render_template('welcome.html', title='welcome')
 
-@app.route('/main', methods=['GET','POST'])
-def main():
+@app.route('/main/<username>', methods=['GET','POST'])
+def main(username):
     return render_template('main.html', title='main')
 
+def validate_input(fields):
+
+    for key in fields:
+        if fields[key] == '':
+            return 'All fields in form must be filled out!'
+
+    if len(fields['password']) < 8:
+        return 'Password is too short!'
+
+    if fields['password'] != fields['conf_password']: 
+        return 'Password not confirmed!'
+
+    return None
+        
 if __name__  == '__main__': 
     app.run()
